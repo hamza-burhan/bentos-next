2282a1dea15aa11a24ba0f1a2099082e2da6c341
Hamza Burhan <123304211+hamza-burhan@users.noreply.github.com>
2025-07-19 22:38:09 +0500

    first section

--- src/components/sections/hero.jsx
+++ src/components/sections/hero.jsx
@@ -1,7 +1,7 @@
 'use client'
 import React from 'react'
 import Link from 'next/link';
-import { RiFacebookCircleFill, RiTwitterXLine, RiLinkedinFill, RiGithubLine, RiCircleFill, RiDownloadLine } from '@remixicon/react'
+import { RiFacebookCircleFill, RiTwitterXLine, RiLinkedinFill, RiGithubLine, RiCircleFill, RiDownloadLine, RiArrowRightUpLine } from '@remixicon/react'
 import PartnersMarquee from './partnersMarquee';
 import SlideUp from '@/utlits/animations/slideUp';
 
@@ -15,14 +15,15 @@ const Hero = () => {
                         <SlideUp>
                             <div className="about-image-part">
                                 <img src={"/images/about/profile.png"} alt="About Me" />
-                                <h2>Bentos Walker</h2>
-                                <p>I am a Web Designer based in san francisco.</p>
+                                <h2>Hamza Burhan</h2>
+                                <p>I am a Full Stack Developer based in Pakistan.</p>
+
                                 <div className="about-social text-center">
                                     <ul>
-                                        <li><Link href=""><RiFacebookCircleFill size={20} /></Link></li>
-                                        <li><Link href=""><RiTwitterXLine size={20} /></Link></li>
-                                        <li><Link href=""><RiLinkedinFill size={20} /></Link></li>
-                                        <li><Link href=""><RiGithubLine size={20} /></Link></li>
+                                        <li><Link href="https://www.facebook.com/hamza.burhan.90"><RiFacebookCircleFill size={20} /></Link></li>
+                                        <li><Link href="https://x.com/HBurhan70941"><RiTwitterXLine size={20} /></Link></li>
+                                        <li><Link href="https://www.linkedin.com/in/hamza-burhan-57223516b/"><RiLinkedinFill size={20} /></Link></li>
+                                        <li><Link href="https://github.com/hamza-burhan"><RiGithubLine size={20} /></Link></li>
                                     </ul>
                                 </div>
                             </div>
@@ -35,15 +36,16 @@ const Hero = () => {
                             <div className="about-content-part">
                                 <p>Hello There!</p>
                                 <h2>
-                                    I’m Bentos Walker, a product designer crafting user-centric design with pixel-perfect precision.
+                                    I’m Hamza Burhan, a softwere engineer building fast, secure web apps that help businesses grow.
                                 </h2>
                                 <div className="adress-field">
                                     <ul>
-                                        <li className='d-flex align-items-center'><i><RiCircleFill size={14} /></i> Available for Freelancing</li>
+                                        <li className='d-flex align-items-center'><i><RiCircleFill size={14} /></i> Available</li>
                                     </ul>
                                 </div>
                                 <div className="hero-btns">
-                                    <Link href="/contact" className="theme-btn">Download CV <i><RiDownloadLine size={16} /></i> </Link>
+                                    <Link href="/works" className="theme-btn">View My Work <i><RiArrowRightUpLine size={16} /></i> </Link>
+                                    <i class="ri-arrow-right-up-line"></i>
                                 </div>
                             </div>
                         </SlideUp>
